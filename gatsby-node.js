5f4d696561a94bee4b32339859179011c9c66e1d
David Fateh <davidfateh@gmail.com>
2022-03-02 19:35:12 -0500

    server side rendering

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,9 +1,32 @@
-exports.createPages = async ({ actions }) => {
+
+exports.createPages = async ({ actions, graphql }) => {
+  const { data } = await graphql(`
+      query {
+        allContentfulPost {
+          nodes {
+            date
+            slug
+            title
+            content {
+              raw
+            }
+          }
+        }
+      }
+    `
+  );
+  console.log(data);
+
   const { createPage } = actions
-  createPage({
-    path: "/using-dsg",
-    component: require.resolve("./src/templates/using-dsg.js"),
-    context: {},
-    defer: true,
+
+  data.allContentfulPost.nodes.forEach(node => {
+    createPage({
+      path: `/${node.slug}`,
+      component: require.resolve("./src/templates/using-dsg.js"),
+      context: {
+        data: { node }
+      },
+      defer: true,
+    })
   })
 }
--- src/templates/using-dsg.js
+++ src/templates/using-dsg.js
@@ -1,23 +1,21 @@
 import * as React from "react"
 import { Link } from "gatsby"
+import { documentToHtmlString } from '@contentful/rich-text-html-renderer';
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
-const UsingDSG = () => (
-  <Layout>
-    <Seo title="Using DSG" />
-    <h1>Hello from a DSG Page</h1>
-    <p>This page is not created until requested by a user.</p>
-    <p>
-      To learn more, head over to our{" "}
-      <a href="https://www.gatsbyjs.com/docs/reference/rendering-options/deferred-static-generation/">
-        documentation about Deferred Static Generation
-      </a>
-      .
-    </p>
-    <Link to="/">Go back to the homepage</Link>
-  </Layout>
-)
+const UsingDSG = ({ pageContext }) => {
+  const { data: { node } } = pageContext;
+  const body = JSON.parse(node.content.raw);
+  return (
+      <Layout>
+        <Seo title={node.title}/>
+        <h1>{node.title}</h1>
+        <div dangerouslySetInnerHTML={{__html: documentToHtmlString(body)}} />
+        <Link to="/">Go back to the homepage</Link>
+      </Layout>
+    );
+}
 
 export default UsingDSG
