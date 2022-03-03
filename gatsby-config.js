66ebae672b2927cc7f961b780f605e8d46296ada
David Fateh <davidfateh@gmail.com>
2022-03-02 19:02:35 -0500

    contentful hookup

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,6 +31,13 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: 'gatsby-source-contentful',
+      options: {
+        spaceId: 'snztxpggk229',
+        accessToken: 'OYzvd0NSZf7MziTnfPA5UHw5iS3nceteFNG-fTe-Oxo',
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
